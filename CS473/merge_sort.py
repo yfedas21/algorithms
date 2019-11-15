26d6f3c3712f51ca153de8f9b3260a8d98bb4375
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2019-11-15 11:52:38 -0800

    Added driver and framework

---
+++ CS473/merge_sort.py
@@ -0,0 +1,27 @@
+# merge_sort algorithm
+
+def merge_sort(A): 
+    # stores the length of the input array
+    # for easy reuse
+    ln = A // 2
+    
+    # 2 sub arrays to hold halves of A
+    B = []; C = []
+
+    # check if len(A) is greater than 2
+    if len(A) > 2: 
+        # copy first half of A into B
+        # and second half into C
+        for i in range()
+
+def merge(A, B, C): 
+    pass
+
+def main(): 
+    in_1 = [3, 5, 7, 9, 8, 6, 4, 2]
+    in_2 = [3, 1, 7, 5, 4, 2, 8, 6, 11, 10, 12, 14, 16, 15, 13, 9]
+
+# code driver, tells the interpreter
+# starting point for execution
+if __name__ == "__main__": 
+    main()
