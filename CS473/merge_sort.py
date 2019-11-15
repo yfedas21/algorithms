c44b03e98e897be9ea435ef2f42b7a10aecf0bbb
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2019-11-15 11:59:27 -0800

    Split array works correctly

--- CS473/merge_sort.py
+++ CS473/merge_sort.py
@@ -3,7 +3,7 @@
 def merge_sort(A): 
     # stores the length of the input array
     # for easy reuse
-    ln = A // 2
+    ln = len(A) // 2
     
     # 2 sub arrays to hold halves of A
     B = []; C = []
@@ -12,14 +12,23 @@ def merge_sort(A):
     if len(A) > 2: 
         # copy first half of A into B
         # and second half into C
-        for i in range()
+        for i in range(ln):
+            B.append(A[i])
+            C.append(A[i + ln])
+    print("B is ", B)
+    print("C is ", C)
 
 def merge(A, B, C): 
     pass
 
+# driver for the program
 def main(): 
     in_1 = [3, 5, 7, 9, 8, 6, 4, 2]
     in_2 = [3, 1, 7, 5, 4, 2, 8, 6, 11, 10, 12, 14, 16, 15, 13, 9]
+
+    # sample
+    merge_sort(in_1)
+    merge_sort(in_2)
 
 # code driver, tells the interpreter
 # starting point for execution
